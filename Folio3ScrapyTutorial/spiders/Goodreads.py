99f33852afadef4e849b8a9059e9df73c3d2d2c4
Talha Saleem <talhasaleem09@hotmail.com>
2019-12-04 17:04:28 +0500

    Initial commit

---
+++ Folio3ScrapyTutorial/spiders/Goodreads.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+class GoodeadsSpider(scrapy.Spider):
+    name = 'Goodreads'
+    allowed_domains = ['goodreads.com']
+    start_urls = ['https://www.goodreads.com/quotes']
+
+    def parse(self, response):
+        with open('quotes.html', 'w', encoding='utf-8') as f:
+            f.write(response.text)
+
+        data = {}
+        data['quotes'] = response.xpath('//div[@class="quoteText"]/text()').extract()
+        
+        yield data
+
+        next_page = response.xpath('//a[@class="next_page"]/@href').extract_first()
+        if next_page:
+            url = 'https://www.goodreads.com' + next_page
+            yield scrapy.Request(url, self.parse)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Folio3ScrapyTutorial
+ Simple Scrapy Tutorial for Folio3 Pvt. Ltd
